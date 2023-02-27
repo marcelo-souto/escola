e8eea5987d1494f8856c9427ab38b6fc7a7149d2
Marcelo Souto <marcelosouto676@gmail.com>
2023-02-27 18:24:02 -0300

    aluno controller

--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const logger = require('./functions/logger.js');
 const path = require('path');
 require('dotenv').config();
@@ -7,18 +7,18 @@ require('dotenv').config();
 const anoRoutes = require('./routes/anoRoutes.js');
 const turnosRoutes = require('./routes/turnosRoutes.js');
 const turmasRoutes = require('./routes/turmasRoutes.js');
-const tokenRouts = require('./routes/tokenRoutes.js')
-const diretorRoutes = require('./routes/diretorRoutes.js')
-const materiaRoutes = require('./routes/materiaRoutes')
-const professorRoutes = require('./routes/professorRoutes')
-
+const tokenRouts = require('./routes/tokenRoutes.js');
+const diretorRoutes = require('./routes/diretorRoutes.js');
+const materiaRoutes = require('./routes/materiaRoutes');
+const professorRoutes = require('./routes/professorRoutes');
+const alunoRoutes = require('./routes/alunoRoutes.js');
 
 // Variaveis
 const port = process.env.PORT;
 const server = express();
 
 // Middlewares
-server.use(cors())
+server.use(cors());
 server.use(express.json());
 server.use(logger);
 
@@ -26,12 +26,11 @@ server.use(logger);
 server.use('/', anoRoutes);
 server.use('/', turnosRoutes);
 server.use('/', turmasRoutes);
-server.use('/', materiaRoutes)
+server.use('/', materiaRoutes);
 server.use('/', tokenRouts);
 server.use('/', diretorRoutes);
-server.use('/', professorRoutes)
-
-
+server.use('/', professorRoutes);
+server.use('/', alunoRoutes);
 
 server.get('/', (req, res) => {
 	return res.send('<h1>Servidor rodando ...<h1>');
